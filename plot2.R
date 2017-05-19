cc47a6e1d85e5caf82bb9f24b359c2a21720b038
wenlytang <wenly.tang@gmail.com>
2017-05-18 22:37:25 -0400

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,7 @@
+mydata <- read.csv.sql("household_power_consumption.txt", "select * from file where Date in ('1/2/2007','2/2/2007')", header = TRUE, sep = ";")
+closeAllConnections()
+mydata$DateTime <- paste(mydata$Date, mydata$Time, sep = " ")
+mydata$DateTime <- strptime(mydata$DateTime, format = "%d/%m/%Y %H:%M:%S")
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+with(mydata, plot(DateTime, Global_active_power, type = "l", xlab = " ", ylab = "Global Active Power (kilowatts)"))
+dev.off()
